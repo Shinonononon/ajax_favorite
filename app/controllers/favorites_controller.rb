b93adb56845d76564e7afe49654ae97019abbc80
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-18 11:34:04 +0900

    aaa

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
-  end
+    @blog = Blog.find(params[:blog_id])
+    current_user.favorites.create(blog_id: @blog.id)
+        #current_user.favorites.create(blog_id: params[:blog_id])
+        #favorite = current_user.favorites(blog_id: params[:blog_id])
+        #favorite.save
+      end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    @blog = @favorite.blog
+    @favorite.destroy
+    #current_user.favorites.find_by(id: params[:id]).destroy
+    #favorite = Favorite.find_by(id: params[:id])
+    #favorite.destroy
   end
 
 end
